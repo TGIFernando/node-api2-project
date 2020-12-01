a12660968b64eae404926a55f0316f564a0f1e42
Fernando Chavez <fjchavez01@gmail.com>
2020-12-01 13:24:39 -0800

    added /api/post/:id endpoint

--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -7,10 +7,25 @@ router.get('/', async (req,res) => {
     const { query } = req
     try {
         const posts = await Util.find(query)
-        res.json(posts)
+        res.status(200).json(posts)
     } catch (error) {
-        res.json(error.message)
+        res.status(400).json(error.message)
     }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    Util.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        }).catch (error => {
+            console.log(error)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
 })
 
 module.exports = router
