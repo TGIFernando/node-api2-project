b05268c03a663382f995d69cb5cc326b9d35efd7
Fernando Chavez <fjchavez01@gmail.com>
2020-12-01 14:15:52 -0800

    added put endpoint

--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -58,4 +58,25 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const changes = req.body
+    if (!changes.title || !changes.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Util.update(id, changes)
+            .then(posts => {
+                if (posts) {
+                    res.status(200).json(posts)
+                } else {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+            }) .catch (error => {
+                console.log(error.message)
+                res.status(500).json({ error: "The post information could not be modified." })
+            })
+    }
+
+})
+
 module.exports = router
